6ba4349c587b6da46a8f853a2a0411c5c338d730
pstrakeljahn <patrick.strakeljahn@gmail.com>
2021-05-13 22:30:33 +0200

    csv export added

--- analyse.php
+++ analyse.php
@@ -1,5 +1,7 @@
 <html>
-<header></header>
+<header>
+<title>Auswertung</title>
+</header>
 <body>
 <?php
 include "settings.php";
@@ -7,27 +9,47 @@ include "settings.php";
 $analyseData = new Analyser();
 $arrAnalyse = $analyseData->analyse();
 echo '<h1>Auswertung der Daten, min Lütt :*</h1>';
-foreach($arrAnalyse as $key => $analyse){
+echo '<div style="float:right; margin-right: 50px;"><a href="analyse.php?csvExport=true">Export CSV-File</a></div>';
+
+if (isset($_GET['csvExport']))
+{
+    $analyseData->createCSV();
+}
+
+foreach ($arrAnalyse as $key => $analyse)
+{
     echo '<h2><b><u>' . $key . "</b></u></h2>";
     echo '
-    <table border="3" width="100">
+    <table border="3">
         <thead>
-            <th width="250">proz. Anteil</th>
-            <th>Stamm und DNS-Sequenz</th>
+            <th style="width:250px;">proz. Anteil</th>
+            <th style="width:400px;">';
+    $title = 'Stamm';
+    if ($analyseData::SHOWDNASEQUENZ)
+    {
+        $title = $title . ' und DNS-Sequenz';
+    }
+    echo $title . '</th>
         </thead>
     ';
-    foreach($analyse as $key => $percent){
+    foreach ($analyse as $key => $percent)
+    {
         $test = explode('__', $key);
         $name = '';
-        for($i = 1; $i < count($test); $i++){
-            $name = $name . ' ' . $test[$i-1];
+        for ($i = 1;$i < count($test);$i++)
+        {
+            $name = $name . ' ' . $test[$i - 1];
         }
-        $dna = $test[count($test)-1];
+        $dna = $test[count($test) - 1];
         echo '
         <tr>
-            <td><b>' . round($percent, $analyseData::DECIMALS) . '<b></td>
-            <td>'. substr($name,1) . '<br>' . $dna . '<td>
-        </tr>';
+            <td style="padding-left:15px;"><b>' . round($percent, $analyseData::DECIMALS) . ' %<b></td>
+            <td>' . substr($name, 1);
+        if ($analyseData::SHOWDNASEQUENZ)
+        {
+            echo '<br>' . $dna;
+        }
+        echo '</td></tr>';
     }
     echo '
     </table><br><br>
--- settings.php
+++ settings.php
@@ -13,22 +13,46 @@ class Analyser
     // SINGLE: Du kannst die Datenpunkte entweder alle einzeln bestimmen (true) oder kumulieren (false).
     // MERGESIZE: Wenn du kumulieren willst musst du angeben wie viele Datensätze zu einer Messung ghören.
     // DECIMALS: Stellt die Nachkommastellen ein.
-
+    // SHOWDNASEQUENZ: Zeigt die DNA Sequenz im Browser an. Sonst nur in der csv
     const FILEPATH = "Gattung_Salami_TB.csv";
     const DATAPOINTS = 6;
     const SINGLE = false;
     const MERGESIZE = 6;
     const DECIMALS = 3;
+    const SHOWDNASEQUENZ = false;
 
     public function analyse()
     {
         // Starte Analyse
         $analyse = new BuildOutputClass();
-        return $analyse->getCSV(
-            self::FILEPATH,
-            self::DATAPOINTS,
-            self::SINGLE,
-            self::MERGESIZE
-        );
+        return $analyse->getCSV(self::FILEPATH, self::DATAPOINTS, self::SINGLE, self::MERGESIZE);
+    }
+
+    public function createCSV()
+    {
+        $arrAnalyse = $this->analyse();
+        $fileContent = [];
+
+        foreach ($arrAnalyse as $key => $value1)
+        {
+            foreach ($value1 as $key2 => $value2)
+            {
+                $fileContent[] = [$key, round($value2, self::DECIMALS) , $key2];
+            }
+        }
+
+        date_default_timezone_set("Europe/Berlin");
+        $timestamp = time();
+        $formatedTime = date("Ymd-Hi", $timestamp);
+
+        $fp = fopen('data_' . $formatedTime . '.csv', 'w');
+
+        foreach ($fileContent as $fields)
+        {
+            fputcsv($fp, $fields, ';');
+        }
+
+        fclose($fp);
     }
 }
+
